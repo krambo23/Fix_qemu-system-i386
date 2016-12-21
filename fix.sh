ed4520a67be614f0f0e0499cb17f36636b8aae28
krambo23 <taj.keshav@gmail.com>
2016-12-21 17:19:26 +0400

    Initial Commit

---
+++ fix.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $1 == "MacOS" ]]; then
+	#cd ~/Library/Preferences/ #Changes directory
+	#rm -r com.android.Emulator.plist #Deletes file
+	echo "Please Relogin"
+elif [[ $1 == "Linux" ]]; then
+	cd ~/.config/Android\ Open\ Source\ Project/ #Changes Directory
+	rm -r Emulator.conf #Deletes file
+	echo "Please Relogin"
+else
+	echo "Wrong OS"
+fi
+
+#Source : https://code.google.com/p/android/issues/detail?id=206786#c7
